879cb8697bf3823528d94ee11ac45fb63ff1d67c
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-06-02 14:20:05 +0800

    fix keyboard scrolling

--- screens/InsertBillDetailScreen.js
+++ screens/InsertBillDetailScreen.js
@@ -9,7 +9,10 @@ import {
   TouchableOpacity,
   View,
   TextInput,
-  ImageBackground
+  ImageBackground,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
 } from 'react-native';
 
 import Constants from 'expo-constants';
@@ -52,131 +55,131 @@ export default function InsertBillDetailScreen() {
     }
   }
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={{ flex: 1, backgroundColor: Colors.headerColor }}>
+        <ImageBackground source={image} style={{ flex: .4, paddingTop: Constants.statusBarHeight, }}>
+          <View style={{ flex: 1 }}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
+              <MaterialIcons name="arrow-back" size={24} color="white" />
+            </TouchableOpacity>
+            <View style={{ height: '100%', justifyContent: 'center' }}>
+              <Text style={{ marginBottom: 70, width: '100%', height: 100, color: 'white', fontSize: 40, fontWeight: 'bold', textAlign: 'center', paddingHorizontal: 20 }}>{title}</Text>
+            </View>
+          </View>
+        </ImageBackground>
+        <View style={{ flex: .8, backgroundColor: 'white', paddingHorizontal: 20, paddingTop: 10 }}>
+          <View style={{ top: -(screenWidth * .28) / 2 }}>
+            <View
+              style={{
+                height: screenWidth * .22,
+                width: screenWidth * .22,
+                backgroundColor: 'white',
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                borderRadius: 10,
+                elevation: 5,
+                alignSelf: 'flex-end'
+              }}
+            >
+              <Image style={{ borderRadius: 10, height: '100%', width: '100%' }} source={{ uri: billObject.image }} />
+            </View>
+            <Text style={{ alignSelf: 'flex-end', marginTop: 5, width: screenWidth * .22, color: 'grey', fontSize: 11, textAlign: 'center' }}>{billObject.companyName}</Text>
 
-    <View style={{ flex: 1, backgroundColor: Colors.headerColor }}>
-      <ImageBackground source={image} style={{ flex: .4, paddingTop: Constants.statusBarHeight, }}>
-        <View style={{ flex: 1 }}>
-          <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
-            <MaterialIcons name="arrow-back" size={24} color="white" />
-          </TouchableOpacity>
-          <View style={{ height: '100%', justifyContent: 'center' }}>
-            <Text style={{ marginBottom: 70, width: '100%', height: 100, color: 'white', fontSize: 40, fontWeight: 'bold', textAlign: 'center', paddingHorizontal: 20 }}>{title}</Text>
+            <TextInput
+              style={{
+                borderRadius: 10,
+                borderColor: 'lightgrey',
+                borderWidth: 1,
+                padding: 10,
+                marginTop: 20,
+                marginBottom: 10,
+              }}
+              maxLength={40}
+              onChangeText={(text) => setBill({ ...bill, ref1: text })}
+              value={bill.ref1}
+              keyboardType='phone-pad'
+              placeholder='Account Number'
+            />
+            {bill.type == 'TELCO' && (
+              <View>
+                <TextInput
+                  style={{
+                    borderRadius: 10,
+                    borderColor: 'lightgrey',
+                    borderWidth: 1,
+                    padding: 10
+                  }}
+                  maxLength={40}
+                  placeholder="Phone Number"
+                  value={bill.ref2}
+                  keyboardType='phone-pad'
+                  onChangeText={(text) => setBill({ ...bill, ref2: text })}
+                />
+              </View>
+            )}
+            {bill.type == 'PERLIS' && (
+              <View >
+                <TextInput
+                  style={{
+                    borderRadius: 10,
+                    borderColor: 'lightgrey',
+                    borderWidth: 1,
+                    padding: 10
+                  }}
+                  maxLength={40}
+                  placeholder="Bill Number"
+                  value={bill.ref2}
+                  keyboardType='phone-pad'
+                  onChangeText={(text) => setBill({ ...bill, ref2: text })}
+                />
+              </View>
+            )}
+            {bill.billerCode === '40386' && (
+              <View >
+                <TextInput
+                  style={{
+                    borderRadius: 10,
+                    borderColor: 'lightgrey',
+                    borderWidth: 1,
+                    padding: 10
+                  }}
+                  maxLength={40}
+                  placeholder="Contract Account Number"
+                  value={bill.ref2}
+                  onChangeText={(text) => setBill({ ...bill, ref2: text })}
+                />
+              </View>
+            )}
+            {bill.billerCode === '37234' && (
+              <View >
+                <TextInput
+                  style={{
+                    borderRadius: 10,
+                    borderColor: 'lightgrey',
+                    borderWidth: 1,
+                    padding: 10
+                  }}
+                  placeholder="Bill Code No"
+                  maxLength={40}
+                  value={bill.ref2}
+                  onChangeText={(text) => setBill({ ...bill, ref2: text })}
+                />
+              </View>
+            )}
+          </View>
+          <View style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'white', paddingBottom: 10, left: 20 }}>
+            <TouchableOpacity style={{ borderWidth: 1, borderColor: 'lightgrey', borderRadius: 10, paddingVertical: 10 }} onPress={createBillPressed}>
+              <Text style={{ fontSize: 20, fontWeight: '600', textAlign: 'center', color: Colors.secondaryColor }}>Next</Text>
+            </TouchableOpacity>
           </View>
         </View>
-      </ImageBackground>
-      <View style={{ flex: .8, backgroundColor: 'white', paddingHorizontal: 20, paddingTop: 10 }}>
-        <View style={{ top: -(screenWidth * .28) / 2 }}>
-          <View
-            style={{
-              height: screenWidth * .22,
-              width: screenWidth * .22,
-              backgroundColor: 'white',
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 2,
-              },
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84,
-              borderRadius: 10,
-              elevation: 5,
-              alignSelf: 'flex-end'
-            }}
-          >
-            <Image style={{ borderRadius: 10, height: '100%', width: '100%' }} source={{ uri: billObject.image }} />
-          </View>
-          <Text style={{ alignSelf: 'flex-end', marginTop: 5, width: screenWidth * .22, color: 'grey', fontSize: 11, textAlign: 'center' }}>{billObject.companyName}</Text>
-
-          <TextInput
-            style={{
-              borderRadius: 10,
-              borderColor: 'lightgrey',
-              borderWidth: 1,
-              padding: 10,
-              marginTop: 20,
-              marginBottom: 10,
-            }}
-            maxLength={40}
-            onChangeText={(text) => setBill({ ...bill, ref1: text })}
-            value={bill.ref1}
-            keyboardType='phone-pad'
-            placeholder='Account Number'
-          />
-          {bill.type == 'TELCO' && (
-            <View>
-              <TextInput
-                style={{
-                  borderRadius: 10,
-                  borderColor: 'lightgrey',
-                  borderWidth: 1,
-                  padding: 10
-                }}
-                maxLength={40}
-                placeholder="Phone Number"
-                value={bill.ref2}
-                keyboardType='phone-pad'
-                onChangeText={(text) => setBill({ ...bill, ref2: text })}
-              />
-            </View>
-          )}
-          {bill.type == 'PERLIS' && (
-            <View >
-              <TextInput
-                style={{
-                  borderRadius: 10,
-                  borderColor: 'lightgrey',
-                  borderWidth: 1,
-                  padding: 10
-                }}
-                maxLength={40}
-                placeholder="Bill Number"
-                value={bill.ref2}
-                keyboardType='phone-pad'
-                onChangeText={(text) => setBill({ ...bill, ref2: text })}
-              />
-            </View>
-          )}
-          {bill.billerCode === '40386' && (
-            <View >
-              <TextInput
-                style={{
-                  borderRadius: 10,
-                  borderColor: 'lightgrey',
-                  borderWidth: 1,
-                  padding: 10
-                }}
-                maxLength={40}
-                placeholder="Contract Account Number"
-                value={bill.ref2}
-                onChangeText={(text) => setBill({ ...bill, ref2: text })}
-              />
-            </View>
-          )}
-          {bill.billerCode === '37234' && (
-            <View >
-              <TextInput
-                style={{
-                  borderRadius: 10,
-                  borderColor: 'lightgrey',
-                  borderWidth: 1,
-                  padding: 10
-                }}
-                placeholder="Bill Code No"
-                maxLength={40}
-                value={bill.ref2}
-                onChangeText={(text) => setBill({ ...bill, ref2: text })}
-              />
-            </View>
-          )}
-        </View>
-        <View style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'white', paddingBottom: 10, left: 20 }}>
-          <TouchableOpacity style={{ borderWidth: 1, borderColor: 'lightgrey', borderRadius: 10, paddingVertical: 10 }} onPress={createBillPressed}>
-            <Text style={{ fontSize: 20, fontWeight: '600', textAlign: 'center', color: Colors.secondaryColor }}>Next</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View >
-
+      </KeyboardAvoidingView >
+    </TouchableWithoutFeedback>
   );
 }
 InsertBillDetailScreen.navigationOptions = {
