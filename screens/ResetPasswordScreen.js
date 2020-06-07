b07f2e02245461dce39826acae314592fd5d2989
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-06-07 18:29:09 +0800

    edit reset password

--- screens/ResetPasswordScreen.js
+++ screens/ResetPasswordScreen.js
@@ -12,6 +12,8 @@ import {
 import { useDispatch } from 'react-redux';
 import userSlice from '../features/accounts/userSlice'
 import Colors from '../constants/Colors'
+import Constants from 'expo-constants';
+
 const userAction = userSlice.actions
 
 export default function LoginScreen({ navigation }) {
@@ -28,25 +30,41 @@ export default function LoginScreen({ navigation }) {
     alert(`An email with a link has been sent to ${email}. Please open the link in your phone.`)
   }
   return (
-    <ScrollView className="scrollView" style={styles.container}>
-      <View>
-        <Text style={{ color: 'white', marginTop: 10 }}>Email:</Text>
+
+    <View style={{
+      paddingHorizontal: 20,
+      backgroundColor: Colors.primaryColor,
+      flex: 1,
+      paddingTop: Constants.statusBarHeight
+    }}>
+      <View style={{ flex: .5, justifyContent: 'center', alignItems: 'center' }}>
+        <Image
+          resizeMode='contain'
+          style={{
+            width: 100,
+            height: 100
+          }}
+          source={require('../assets/images/icon.png')} />
+        <Text style={{ marginLeft: 5, fontWeight: '600', color: 'white', fontSize: 30, marginTop: 10 }}>Bayaq</Text>
+
+      </View>
+      <View style={{ flex: .5 }}>
         <TextInput
           maxLength={40}
           onChangeText={(text) => setEmail(text)}
           value={email}
+          placeholder='Email'
+          placeholderTextColor="lightgrey"
           autoCapitalize='none'
           keyboardType='email-address'
-          style={{ color: 'white', borderColor: 'white', marginTop: 10, borderWidth: 1, paddingVertical: 5, paddingHorizontal: 10 }}
+          style={{ borderRadius: 5, color: 'white', borderColor: 'white', marginTop: 10, borderWidth: 1, paddingVertical: 5, paddingHorizontal: 10 }}
         />
-
-
-        <TouchableOpacity style={{ marginTop: 20, marginBottom: 10, borderWidth: 1, borderColor: 'white', paddingVertical: 5 }} onPress={submitPressed}>
-          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Request Reset Password</Text>
+        <TouchableOpacity style={{ borderRadius: 5, marginTop: 20, marginBottom: 10, borderWidth: 1, borderColor: 'white', paddingVertical: 5 }} onPress={submitPressed}>
+          <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Reset Password</Text>
         </TouchableOpacity>
 
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
