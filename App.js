6f7bfd00b71416855fb8b1050b143b6a85ae5b3c
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-06-20 16:02:27 +0800

    add bills and change status bar

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { AppLoading, Linking } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 import { configureStore } from '@reduxjs/toolkit'
 import AppNavigator from './navigation/AppNavigator';
@@ -49,7 +50,7 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'ios' && <StatusBar style="light" />}
         <Provider store={store}>
           <AppNavigator />
         </Provider>
